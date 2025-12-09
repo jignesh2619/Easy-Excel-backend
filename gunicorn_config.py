d4b7529f37995b62b1c460469adf5ad073807bd9
jignesh2619 <136894974+jignesh2619@users.noreply.github.com>
2025-12-09 16:12:27 +0530

    Add missing gunicorn_config.py file

---
+++ gunicorn_config.py
@@ -0,0 +1,38 @@
+"""
+Gunicorn configuration for LazyExcel Backend
+"""
+import multiprocessing
+import os
+
+# Server socket
+bind = "127.0.0.1:8000"
+backlog = 2048
+
+# Worker processes
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = "uvicorn.workers.UvicornWorker"
+worker_connections = 1000
+timeout = 120
+keepalive = 5
+
+# Logging
+accesslog = "-"
+errorlog = "-"
+loglevel = "info"
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(D)s'
+
+# Process naming
+proc_name = "lazyexcel-backend"
+
+# Server mechanics
+daemon = False
+pidfile = None
+umask = 0
+user = None
+group = None
+tmp_upload_dir = None
+
+# SSL (if needed)
+keyfile = None
+certfile = None
+
